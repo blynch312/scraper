8ab4cfe07c79826eb90e1cf3b294f09df429cd58
Brian Lynch <brianlynch@Brians-MacBook-Pro.local>
2018-07-30 20:59:21 -0400

    fixed db

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
+
 var db = require("./models");
 
 var PORT = 3000;
@@ -30,20 +31,25 @@ app.use(express.static("public"));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
 
+// mongoose.Promise = Promise;
+// mongoose.connect(MONGODB_URI, {
+//     useMongoClient: true
+// });
+
 // Routes
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
+    console.log("scrape started!")
     // First, we grab the body of the html with request
-    axios.get("http://www.echojs.com/").then(function (response) {
+    axios.get("https://www.nbcsports.com/").then(function (response) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
 
         // Now, we grab every h2 within an article tag, and do the following:
-        $("article h2").each(function (i, element) {
+        $("ul li").each(function (i, element) {
             // Save an empty result object
             var result = {};
-
             // Add the text and href of every link, and save them as properties of the result object
             result.title = $(this)
                 .children("a")
@@ -52,11 +58,19 @@ app.get("/scrape", function (req, res) {
                 .children("a")
                 .attr("href");
 
+            // Add the text and href of every link, and save them as properties of the result object
+            // result.title = $(this)
+            //     .children("a")
+            //     .text();
+            // result.link = $(this)
+            //     .children("a")
+            //     .attr("href");
+
             // Create a new Article using the `result` object built from scraping
             db.Article.create(result)
                 .then(function (dbArticle) {
                     // View the added result in the console
-                    console.log(dbArticle);
+                    // console.log(dbArticle);
                 })
                 .catch(function (err) {
                     // If an error occurred, send it to the client
